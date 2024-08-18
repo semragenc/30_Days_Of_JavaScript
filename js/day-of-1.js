da425dbb020b8915d7dbe4f498ce6714f3173adb
Semra Genc <semrademir292@gmail.com>
2024-08-19 00:08:25 +0300

    day one

---
+++ js/day-of-1.js
@@ -0,0 +1,21 @@
+console.log("30 Günde Javascript'e Hoşgeldiniz.")
+
+console.log("Hello", "World", "!");
+console.log("HAPPY", "NEW", "YEAR", 2020);
+console.log("Welcome", "to", 30, "Days", "Of", "JavaScript");
+// Bu ilk yorum
+// Bu ikinci yorum
+// Ben tek satırlık bir yorumum
+
+/*
+ Bu çok satırlı bir yorumdur
+ Çok satırlı yorumlar birden çok satır alabilir 
+ JavaScript web'in dilidir  
+ */
+
+ console.log(2 + 3); // Toplama
+console.log(3 - 2); // Çıkarma
+console.log(2 * 3); // Çarpma
+console.log(3 / 2); // Bölme
+console.log(3 % 2); // Mod Alma
+console.log(3 ** 2); // ;Üs Alma 3 ** 2 == 3 * 3
