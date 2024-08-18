db886761e57356fb2a211e2cea2f8e2fc26dcfc5
Semra Genc <semrademir292@gmail.com>
2024-08-19 00:30:50 +0300

    day one finish

--- js/day-of-1.js
+++ js/day-of-1.js
@@ -19,3 +19,42 @@ console.log(2 * 3); // Çarpma
 console.log(3 / 2); // Bölme
 console.log(3 % 2); // Mod Alma
 console.log(3 ** 2); // ;Üs Alma 3 ** 2 == 3 * 3
+
+
+
+// Farklı veri türlerinin, farklı değişkenlerini tanımlama
+let ad = "Semra"; // bir kişinin adı
+let soyAd = 'Genç'; // bir kişinni soyadı
+let ulke = "Türkiye"; // ülke
+let sehir = "Ankara"; // başkent
+let yas = 27; // yaş
+let instagram = "@ssgg" // instagram adresi
+let evliMi = true;
+
+console.log(ad, soyAd, ulke, sehir, yas, instagram, evliMi);
+
+
+// Değişkenlere sayı değerleri tanımlama
+let yass = 100; // yas
+const yerCekim = 9.81; // m/s2 cinsinden yer çekimi değeri
+const kaynamaNoktasi = 100; // °C cinsinden suyun kaynama noktası
+const PI = 3.14; // geometrik sabit
+console.log(yerCekim, kaynamaNoktasi, PI);
+
+// Birden fazla değişkenleri virgül ile ayırarak tek satırda tanımlayabiliriz. Ancak ayrı satırlarda tanımlamak, kodu daha okunabilir kıldığı için tavsiye ederim.
+let isim = "SemraGenç",
+  is = "Öğretmen",
+  konum = "Boraniyee";
+console.log(isim, is, konum);
+
+
+let petAnimal;
+console.log(petAnimal)
+
+petAnimal = "Ketchup";
+console.log(petAnimal)
+
+const benimYasim = "Ben 25 Yaşındayım." 
+console.log(benimYasim)
+const seninYasin = "Sen 30 Yaşındasın."
+console.log(seninYasin)
